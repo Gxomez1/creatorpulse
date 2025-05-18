acae7b9261ffe96da8bdb4f0138db34a7e22f13d
Lester <199312826+Gxomez1@users.noreply.github.com>
2025-05-18 13:32:03 -0700

    Added creators to sitemap

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,25 +1,49 @@
 export default function handler(req, res) {
-    const baseUrl = 'https://spicyrate.app';
+    const baseUrl = "https://spicyrate.app";
+  
     const staticPages = [
-      '',
-      '/about',
-      '/add-creator',
+      "", // homepage
+      "about",
+      "add-creator",
+    ];
+  
+    const creatorSlugs = [
+      "abella-danger",
+      "alina-rose",
+      "summer-xiris",
+      "ava-reyes",
+      "aishah-sofey",
+      "sophie-rain",
+      "lana-lux",
+      "brandi-costarica",
+      "daiana-doll",
+      "angelica-bliss",
+      // Add more here manually, or auto-fetch later from Firestore
     ];
   
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${staticPages
-          .map(
-            (page) => `
-          <url>
-            <loc>${baseUrl}${page}</loc>
-          </url>
-        `
-          )
-          .join('')}
-      </urlset>`;
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${staticPages
+      .map((page) => {
+        return `
+      <url>
+        <loc>${baseUrl}/${page}</loc>
+      </url>
+    `;
+      })
+      .join("")}
+    ${creatorSlugs
+      .map((slug) => {
+        return `
+      <url>
+        <loc>${baseUrl}/creator/${slug}</loc>
+      </url>
+    `;
+      })
+      .join("")}
+  </urlset>`;
   
-    res.setHeader('Content-Type', 'application/xml');
+    res.setHeader("Content-Type", "application/xml");
     res.status(200).end(sitemap);
   }
   
